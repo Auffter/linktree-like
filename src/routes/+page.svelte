1b3b65897d88d779b303a997abf7888f4726508d
Auffter <lukikarpiel@gmail.com>
2024-07-15 12:05:58 +0200

    Added support for daisyUI

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,4 @@
 <h1 class="text-red-400">Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<button class="btn btn-blue">Click me</button>
