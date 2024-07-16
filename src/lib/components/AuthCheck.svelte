21b33a917b217b74461a2f1bc2d799444d126d13
Auffter <lukikarpiel@gmail.com>
2024-07-16 15:41:07 +0200

    Added authcheck component for handling available content for users

---
+++ src/lib/components/AuthCheck.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import { user } from "$lib/firebase";
+</script>
+
+{#if $user}
+    <slot />
+{:else}
+    <p class="text-error">
+        You are not logged in. Please sign in to continue.
+        <a href="/login" class="btn btn-primary">Sign in</a>
+    </p>
+{/if}
