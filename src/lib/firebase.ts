85e7a86e9df3b1e69d7c19eeb0655f51b02f2d69
Auffter <lukikarpiel@gmail.com>
2024-07-16 11:15:55 +0200

    exported functions that other modules will import and use

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -20,8 +20,8 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-const auth = getAuth(app);
-const storage = getStorage(app);
-const analytics = getAnalytics(app);
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
+export const storage = getStorage(app);
+//export const analytics = getAnalytics(app);
