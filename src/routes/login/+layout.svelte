83c292182066d0eebd4abc80549b089ff8d283f0
Auffter <lukikarpiel@gmail.com>
2024-07-16 11:16:46 +0200

    Added buttons that help to navigate through pages

--- src/routes/login/+layout.svelte
+++ src/routes/login/+layout.svelte
@@ -1,24 +1,26 @@
 <script lang="ts">
-  import { page } from "$app/stores";
+    import { page } from "$app/stores";
     import AnimatedRoute from "$lib/components/AnimatedRoute.svelte";
 </script>
 
+<a href="/" class="btn">Home</a>
+
 <nav class="flex justify-center my-6">
-  <ul class="steps">
+    <ul class="steps">
     <a href="/login" class="step step-primary">Sign In</a>
     <a
-      href="/login/username"
-      class="step"
-      class:step-primary={$page.route.id?.match(/username|photo/g)}>
-      Choose Username
+        href="/login/username"
+        class="step"
+        class:step-primary={$page.route.id?.match(/username|photo/g)}>
+        Choose Username
     </a>
     <a
-      href="/login/photo"
-      class="step"
-      class:step-primary={$page.route.id?.includes("photo")}>
-      Upload Photo
+        href="/login/photo"
+        class="step"
+        class:step-primary={$page.route.id?.includes("photo")}>
+        Upload Photo
     </a>
-  </ul>
+    </ul>
 </nav>
 
 <AnimatedRoute>    
