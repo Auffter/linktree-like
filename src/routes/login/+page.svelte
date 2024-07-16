b14e7d27ec7bd68e874e30666f3107f1ee4de1cf
Auffter <lukikarpiel@gmail.com>
2024-07-16 11:22:22 +0200

    Added feature that allows to sign in with google

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,5 +1,14 @@
 <script lang="ts">
-    
+    import { auth } from "$lib/firebase";
+    import { GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
+
+    async function signInWithGoogle() {
+    const provider = new GoogleAuthProvider();
+    const user = await signInWithPopup(auth, provider);
+    console.log(user);
+}
 </script>
 
-<h1>Login</h1>
+<h2>Login</h2>
+
+<button class="btn btn-accent" on:click={signInWithGoogle}>Sign in with Google</button>
