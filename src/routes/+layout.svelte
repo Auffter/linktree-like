424ec1c3bea9dc0929e29a9da5bfadcdd5565d8c
Auffter <lukikarpiel@gmail.com>
2024-07-18 11:57:01 +0200

    Added global subscription to user data in root layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,10 @@
 <script lang="ts">
     import "../app.css"
+    import {user, userData} from "$lib/firebase"
+
+    // Global subscription to the user data
+    $user
+    $userData
 </script>
 
 <div class="min-h-screen flex flex-col">
