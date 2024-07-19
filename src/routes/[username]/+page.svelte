22d1c4128d299dae0d9a6721ca62294742139a34
Auffter <lukikarpiel@gmail.com>
2024-07-19 11:54:24 +0200

    Added page file that displays data from firebase as profile unique to each user

---
+++ src/routes/[username]/+page.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+    import type { PageData } from './$types';
+    
+    export let data: PageData;
+</script>
+
+<svelte:head>
+    <title>{data.username} profile</title>
+    <meta name="description" content="{data.bio}" />
+</svelte:head>
+
+<main class="prose text-center mx-auto">
+    <h1 class="text-7xl text-purple-500 mt-4">
+        {data.username}
+    </h1>
+
+    <img
+        src={data.photoURL ?? '/userDefaultIcon.webp'}
+        alt="photoURL"
+        class="mx-auto mt-4"
+        width="256"
+        height="256"
+    />
+
+    <p class="text-xl mt-4">{data.bio ?? "No bio yet..."}</p>
+    <ul class="list-none">
+        {#each data.links as item}
+            {@debug item}
+        {/each}
+    </ul>
+</main>
