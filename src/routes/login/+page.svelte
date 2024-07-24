06053639ca6ad571f49f8e7a8a3f03e326b59b8a
Auffter <lukikarpiel@gmail.com>
2024-07-24 11:12:25 +0200

    Refactor login page to assign session cookie to user upon login, and delete it when signed out

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -3,16 +3,33 @@
     import { GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
 
     async function signInWithGoogle() {
-    const provider = new GoogleAuthProvider();
-    const user = await signInWithPopup(auth, provider);
-    console.log(user);
+        const provider = new GoogleAuthProvider();
+        const loginData = await signInWithPopup(auth, provider);
+        
+        const idToken = await loginData.user.getIdToken();
+
+        const res = await fetch("/api/signin", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ idToken }),
+    });
 }
+
+    async function signOutSSR() {
+        const res = await fetch("/api/signin", {
+            method: "DELETE",
+        });
+        await signOut(auth);
+    };
+
 </script>
 
 {#if $user}
     <h2>Hi, {$user.displayName}</h2>
     <p class="text-center text-success">You are logged in</p>
-    <button class="btn btn-error" on:click={() => signOut(auth)}>Sign out</button>
+    <button class="btn btn-error" on:click={signOutSSR}>Sign out</button>
 {:else}
-<button class="btn btn-accent" on:click={signInWithGoogle}>Sign in with Google</button>
+    <button class="btn btn-accent" on:click={signInWithGoogle}>Sign in with Google</button>
 {/if}
