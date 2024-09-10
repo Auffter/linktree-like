0d46fc5ddf156660b129005bae4e24a079d0b500
Auffter <lukikarpiel@gmail.com>
2024-09-10 18:56:17 +0200

    Added action to handle bio form

--- src/routes/[username]/bio/+page.server.ts
+++ src/routes/[username]/bio/+page.server.ts
@@ -1,15 +1,16 @@
-import type { PageServerLoad } from "./$types";
+import type { PageServerLoad, Actions } from "./$types";
 import { adminAuth, adminDB } from "$lib/server/admin";
-import { error, redirect } from "@sveltejs/kit";
+import { error, redirect, fail } from "@sveltejs/kit";
 
 export const load = (async ({ locals, params }) => {
-    const uid = locals.userID;
     
+    const uid = locals.userID;
+
     if(!uid) {
         throw redirect(301, "/login");
     }
 
-    const userDoc = await adminDB.collection("users").doc(uid!).get();
+    const userDoc = await adminDB.collection("users").doc(uid).get();
     const { username, bio } = userDoc.data()!;
 
     if(params.username !== username) {
@@ -20,3 +21,27 @@ export const load = (async ({ locals, params }) => {
         bio,
     };
 }) satisfies PageServerLoad;
+
+export const actions = {
+    default: async ({ locals, request, params }) => {
+        const uid = locals.userID;
+
+        const data = await request.formData();
+        const bio = data.get('bio');
+
+        const userRef = adminDB.collection("users").doc(uid!);
+        const { username } = (await userRef.get()).data()!;
+
+        if(params.username !== username) {
+            throw error(401, "Unauthorized access");
+        }
+
+        if((bio as string).length > 300) {
+            return fail(400, {problem: "Bio must be less than 300 characters"});
+        }
+
+        await userRef.update({
+            bio,
+        });
+    },
+} satisfies Actions;
