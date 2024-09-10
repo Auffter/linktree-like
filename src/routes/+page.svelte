561ecc60e14d51945a0ac4bcac1f35a692555d8a
Auffter <lukikarpiel@gmail.com>
2024-09-10 18:49:44 +0200

    Added nav buttons

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,15 @@
+<script>
+    import { userData, user } from "$lib/firebase";
+</script>
+
 <a href="/login" class="btn btn-blue">Login</a>
+
+{#if $userData}
+    <ul class="list-none mt-4">
+        <li>
+            <a href= "/{$userData?.username}/edit" class="btn btn-blue flex">Edit Profile</a>
+            <a href="/{$userData?.username}/bio" class="btn btn-blue flex">Edit Bio</a>
+        </li>
+    </ul>
+{/if}
+
